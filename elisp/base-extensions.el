761e284b44c209c55d594ab9a1c5e82d664bd1ae
nasoudnead <nasoundead@163.com>
2017-02-23 22:24:11 +0800

    c

--- elisp/base-extensions.el
+++ elisp/base-extensions.el
@@ -194,8 +194,8 @@
   :init
     (progn
       (show-smartparens-global-mode t)
-      (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
-      (add-hook 'markdown-mode-hook 'turn-on-smartparens-strict-mode)))
+      (add-hook 'prog-mode-hook 'turn-on-smartparens-mode)
+      (add-hook 'markdown-mode-hook 'turn-on-smartparens-mode)))
 
 
 
