fca83d16d322d60012c7ee19688186afe817082c
nasoudnead <nasoundead@163.com>
2017-03-31 22:33:57 +0800

    a

--- elisp/base-theme.el
+++ elisp/base-theme.el
@@ -14,8 +14,8 @@
 ;;   (load-theme 'gruvbox t))
 ;; (load-theme 'manoj-dark t)
 
-;; (require 'hober-theme)
-;; (load-theme 'hober t)
+(require 'hober-theme)
+(load-theme 'hober t)
 ;; (require 'eclipse-theme)
 ;; (load-theme 'eclipse t)
 
@@ -24,10 +24,18 @@
 ;;   :defer t
 ;;   :init
 ;;   (load-theme 'molokai t))
-(use-package gruber-darker-theme
-  :defer t
-  :init
-  (load-theme 'gruber-darker t))
+;; (use-package gruber-darker-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'gruber-darker t))
+;; (use-package zenburn-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'zenburn t))
+;; (use-package github-theme
+;;   :defer t
+;;   :init
+;;   (load-theme 'github))
 
 ;; (use-package madhat2r-theme
 ;;   :defer t
@@ -50,19 +58,19 @@
 ;;   (spaceline-toggle-window-number-off)
 ;;   ; (spaceline-toggle-buffer-size-off)
 ;;   )
-(use-package telephone-line
-  :init
-  (setq telephone-line-lhs
-        '((evil   . (telephone-line-evil-tag-segment))
-          (accent . (telephone-line-vc-segment
-                     telephone-line-erc-modified-channels-segment
-                     telephone-line-process-segment))
-          (nil    . (telephone-line-minor-mode-segment
-                     telephone-line-buffer-segment))))
-  (setq telephone-line-rhs
-        '((nil    . (telephone-line-misc-info-segment))
-          (accent . (telephone-line-major-mode-segment))
-          (evil   . (telephone-line-airline-position-segment))))
+;; (use-package telephone-line
+;;   :init
+;;   (setq telephone-line-lhs
+;;         '((evil   . (telephone-line-evil-tag-segment))
+;;           (accent . (telephone-line-vc-segment
+;;                      telephone-line-erc-modified-channels-segment
+;;                      telephone-line-process-segment))
+;;           (nil    . (telephone-line-minor-mode-segment
+;;                      telephone-line-buffer-segment))))
+;;   (setq telephone-line-rhs
+;;         '((nil    . (telephone-line-misc-info-segment))
+;;           (accent . (telephone-line-major-mode-segment))
+;;           (evil   . (telephone-line-airline-position-segment))))
 
-  (telephone-line-mode t))
+;;   (telephone-line-mode t))
 (provide 'base-theme)
