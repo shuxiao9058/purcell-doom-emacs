60a1767ae2a6d35e21e59482168f58a6df03635d
nasoundead <nasoundead@163.com>
2017-06-20 23:19:43 +0800

    a

--- chinese-fonts-setup/v4/profile1.el
+++ chinese-fonts-setup/v4/profile1.el
@@ -1,3 +1,19 @@
+(defvar my-line-spacing-alist
+  '((9 . 0.1) (10 . 0.1) (11.5 . 0.2)
+    (12.5 . 0.2) (14 . 0.2) (16 . 0.2)
+    (18 . 0.2) (20 . 0.2) (22 . 0.2)
+    (24 . 0.2) (26 . 0.2) (28 . 0.2)
+    (30 . 0.2) (32 . 0.2)))
+
+(defun my-line-spacing-setup (fontsizes-list)
+  (let ((fontsize (car fontsizes-list))
+        (line-spacing-alist (copy-list my-line-spacing-alist)))
+    (dolist (list line-spacing-alist)
+      (when (= fontsize (car list))
+        (setq line-spacing-alist nil)
+        (setq-default line-spacing (cdr list))))))
+
+(add-hook 'cfs-set-font-finish-hook #'my-line-spacing-setup)
 ;;; `cfs--custom-set-fontsnames' 列表有3个子列表，第1个为英文字体列表，第2个为中文字体列表，
 ;;; 第3个列表中的字体用于显示不常用汉字，每一个字体列表中，*第一个* *有效并可用* 的字体将被使用。
 ;;; 将光标移动到上述列表中，按 `C-c C-c' 可以测试字体显示效果。另外，用户可以通过命令
@@ -13,9 +29,10 @@
 ;;; 将光标移动到各个数字上，按 C-c C-c 查看光标处字号的对齐效果。
 ;;; 按 C-<up> 增大光标处字号，按 C-<down> 减小光标处字号。
 (setq cfs--custom-set-fontsizes
-      '(
+      '((9 10.5 10.5)
         (9    10.5 10.5)
         (10   12.0 12.0)
+	(10   12.0 12.0)
         (11.5 13.5 13.5)
         (12.5 15.0 15.0)
         (14   16.5 16.5)
