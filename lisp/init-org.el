94107eb3614e4005cf80ab81acb82b75b9850642
nasoundead <nasoundead@163.com>
2017-11-22 23:14:43 +0800

    ob-ipython

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,4 +1,4 @@
-
+;; init-org.el
 (use-package org
   :config
   (setq org-directory "~/org-files"
@@ -18,7 +18,7 @@
    (ditaa , t)
    (dot . t)
    (emacs-lisp . t)
-   (python . t)
+   (ipython . t)
    (ruby . t)
    (gnuplot . t)
    (sh . t)
@@ -29,7 +29,7 @@
 (setq org-plantuml-jar-path
       (expand-file-name "~/forwin/bin/plantuml.jar"))
 (setq org-ditaa-jar-path (format "%s%s" ""
-				 (expand-file-name "~/forwin/bin/ditaa.jar")) )
+                                 (expand-file-name "~/forwin/bin/ditaa.jar")) )
 
 ;; Enable plantuml-mode for PlantUML files
 (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
@@ -50,51 +50,11 @@
   :config
   (org-projectile:per-repo)
   (setq org-projectile:per-repo-filename "todo.org"
-	org-agenda-files (append org-agenda-files (org-projectile:todo-files))))
+        org-agenda-files (append org-agenda-files (org-projectile:todo-files))))
 
-(require-package 'org-bullets)
-(require 'org-bullets)
-;; (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
-
-
-;;evil-org-mode , git clone git://github.com/edwtjo/evil-org-mode.git
-;; Here are the keys introduced by evil-org
-;; gh	outline-up-heading
-;; gj	org-forward-heading-same-level
-;; gk	org-backward-heading-same-level
-;; gl	outline-next-visible-heading
-;; t	org-todo
-;; T	org-insert-todo-heading nil
-;; H	org-shiftleft
-;; J	org-shiftdown
-;; K	org-shiftup
-;; L	org-shiftright
-;; o	always-insert-item
-;; O	org-insert-heading
-;; ’$’	org-end-of-line
-;; ’^’	org-beginning-of-line
-;; <	org-metaleft
-;; >	org-metaright
-;; <leader>a	org-agenda
-;; <leader>t	org-show-todo-tree
-;; <leader>c	org-archive-subtree
-;; <leader>l	evil-org-open-links
-;; <leader>o	evil-org-recompute-clocks
-;; TAB	org-cycle
-;; M-l	org-metaright
-;; M-h	org-metaleft
-;; M-k	org-metaup
-;; M-j	org-metadown
-;; M-L	org-shiftmetaright
-;; M-H	org-shiftmetaleft
-;; M-K	org-shiftmetaup
-;; M-J	org-shiftmetadown
-;; M-o	org-insert-heading+org-metaright
-;; M-t	org-insert-todo-heading nil+ org-metaright
-(use-package evil-org
-  :ensure t
-  :diminish
-  evil-org-mode)
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 
 (provide 'init-org)
